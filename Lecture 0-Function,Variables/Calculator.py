837f315fbc669304fb4f97aa0aa00963111e798b
Ehtisham Akmal <ehtishamakmal171@gmail.com>
2026-05-28 17:45:05 +0500

    Add files via upload

---
+++ Lecture 0-Function,Variables/Calculator.py
@@ -0,0 +1,43 @@
+
+#Integers
+
+# x=input("what is x?")
+# y=input("what is y?")
+# z=x+y # + this sign is showing catcatenation. joining of two strings
+# print(z)  # this is wrong because it is showing catcatenation not addition.
+# we will have to add integers in it
+# x=input("what is your x?")
+# y=input("what is your y?")
+# z=int(x) + int(y)
+# print(z)
+
+
+# x=int(input("what's your x?"))
+# y=int(input("what's your y?")) This is a better version of the code to avoide bugs
+# print(x+y)
+
+#Floating Numbers
+
+# x=float(input("what's your x?"))
+# y=float(input("what's your y?")) 
+# z=x+y
+# z=round(x+y)
+# print(f"{z:,}")
+
+#Division
+
+# x=float(input("what's your x?"))
+# y=float(input("what's your y?")) 
+# z=x/y
+# print(z)
+
+# x=float(input("what's your x?"))
+# y=float(input("what's your y?")) 
+# z=round(x/y,2)
+# print(z)
+
+# x=float(input("what's your x?"))
+# y=float(input("what's your y?")) 
+# z=x/y
+# print(f"{z:.2f}") This is ver weird or strange code
+
---
+++ Lecture 0-Function,Variables/def.py
@@ -0,0 +1,32 @@
+# name=input("What's your name?")
+# hello()
+# print(name) it will not work because function does not exit.
+
+# def hello():
+#     print("hello")
+    
+# name=input("what's your name?")
+# hello()
+# print(name)
+
+# def hello(to="World"):
+#     print("hello,", to)
+    
+# hello()    
+# name=input("what's your name?")
+# hello(name)
+
+# def main():
+#     x = int (input("what's x?"))
+#     print("x squared is", square(x))            
+
+# def square (n):
+#     return n*n
+# main()
+
+# def main():
+#     x=input("What's your Name?")
+#     print(f"Welcome boy,{x}")
+
+# main()
+
